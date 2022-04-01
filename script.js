23b5210f7493b032a2d5aba73e8bafe9777832b0
Glitch (glitch-blank-website) <none>
2022-04-01 07:10:00 +0000

    💐📉 Checkpoint ./script.js:43427363/1 ./index.html:43427363/201

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [3, 1, 4, 3,5, 2, 1, 2, 4,5];
+var pattern = [3, 1, 4, 3, 5, 2, 1, 2, 4,5];
 var progress= 0;
 var gamePlaying = false;
 var tonePlaying = false;
