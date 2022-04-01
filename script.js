33ebc2b54da3dc87c8c6326a42a8ca15cc2325ef
Glitch (glitch-blank-website) <none>
2022-04-01 04:30:00 +0000

    ⛈🛢 Checkpoint ./script.js:43427363/18 ./index.html:43427363/108 ./style.css:43427363/181

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [3, 1, 4, 3, 2, 1, 2, 4];
+var pattern = [3, 1, 4, 3,5, 2, 1, 2, 4,5];
 var progress= 0;
 var gamePlaying = false;
 var tonePlaying = false;
@@ -90,7 +90,8 @@ const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 392,
-  4: 466.2
+  4: 466.2,
+  5: 550
 }
 function playTone(btn,len){ 
   o.frequency.value = freqMap[btn]
